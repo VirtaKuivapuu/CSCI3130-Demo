bb997f12b1f2e11a2c445a5906b7d8c596b1ca60
Yuxuan Zhao <virta_kuivapuu@hotmail.com>
2018-01-23 21:32:04 -0400

    Add a button and a text field to the UI. The user should input text on the field and click the button after. The text from the field should then be copied to the textbox that was already in the layout.

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String Message = "Hello World";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        EditText editText = findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+
     }
 }
